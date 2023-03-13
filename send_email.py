c8310a0032fb9654ef6e98c0dba2a2abb3dfc933
hakkimS <84006367+hakkimS@users.noreply.github.com>
2023-03-13 09:04:10 +0530

    Update send_email.py

--- send_email.py
+++ send_email.py
@@ -5,10 +5,8 @@ from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 
 try:
-  #email_user = os.environ.get('EMAIL_USER')
-  #email_pass = os.environ.get('EMAIL_PASS')
-  email_user = 'abdhulhakkim0@gmail.com'
-  email_pass = 'scydcayxqieyufzj'
+  email_user = os.environ.get('EMAIL_USER')
+  email_pass = os.environ.get('EMAIL_PASS')
   
   print("hai ji")
   print(email_user, flush=True)
