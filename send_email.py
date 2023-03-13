a42c705eb53ef389966d75e2b05137adef102fba
hakkimS <84006367+hakkimS@users.noreply.github.com>
2023-03-13 08:30:40 +0530

    Update send_email.py

--- send_email.py
+++ send_email.py
@@ -3,22 +3,27 @@ import os
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 
-email_user = os.environ.get('EMAIL_USER')
-email_pass = os.environ.get('EMAIL_PASS')
+try:
+  email_user = os.environ.get('EMAIL_USER')
+  email_pass = os.environ.get('EMAIL_PASS')
 
-print(email_user)
+  print(email_user)
 
-msg = MIMEMultipart()
-msg['From'] = email_user
-msg['To'] = 'abdhulhakkim786@gmail.com'
-msg['Subject'] = 'GitHub push notification'
+  msg = MIMEMultipart()
+  msg['From'] = email_user
+  msg['To'] = 'abdhulhakkim786@gmail.com'
+  msg['Subject'] = 'GitHub push notification'
 
-body = 'A push event has occurred in the main branch!'
-msg.attach(MIMEText(body, 'plain'))
+  body = 'A push event has occurred in the main branch!'
+  msg.attach(MIMEText(body, 'plain'))
 
-server = smtplib.SMTP('smtp.gmail.com', 587)
-server.starttls()
-server.login(email_user, email_pass)
-text = msg.as_string()
-server.sendmail(email_user, 'abdhulhakkim786@gmail.com', text)
-server.quit()
+  server = smtplib.SMTP('smtp.gmail.com', 587)
+  server.starttls()
+  server.login(email_user, email_pass)
+  text = msg.as_string()
+  server.sendmail(email_user, 'abdhulhakkim786@gmail.com', text)
+  server.quit()
+  
+except Exception as e:
+  print(f"Error: {e}", file=sys.stderr)
+  sys.exit(1)
